5f215e2b1ace9858a8be599baedd3cf019ba0c7d
omar <3mar153153@gmail.com>
2025-12-19 17:01:49 +0200

    Restructre backend

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,9 @@ const controller = require("../controllers/auth.controller");
 
 const router = express.Router();
 
+// router.post("/register", controller.register);
 router.post("/register", controller.register);
+router.post("/verify-email", controller.verifyEmail);
 router.post("/login", controller.login);
 router.post("/forgot-password", controller.forgotPassword);
 router.post("/reset-password", controller.resetPassword);
